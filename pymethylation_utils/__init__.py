a3942d692be24d60c7f0fd7d8964171f805a6479
SebastianDall <semoda@bio.aau.dk>
2024-11-30 12:50:53 +0100

    Bump version: 0.2.5 → 0.2.6

--- pymethylation_utils/__init__.py
+++ pymethylation_utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "v0.2.5"
+__version__ = "v0.2.6"
 
 import os
 import platform
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymethylation_utils",
-    version="v0.2.5",
+    version="v0.2.6",
     description="Python wrapper for the methylation_utils Rust binary",
     author="Sebastian Dall",
     author_email="semoda@bio.aau.dk",
